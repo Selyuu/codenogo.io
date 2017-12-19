436133349f04845ec266dbff27af7b46be38c7cf
Selyuu <rainierlouis@gmail.com>
2017-12-19 19:26:43 +0100

    feat: reducers implemented

--- client/src/containers/reducers.js
+++ client/src/containers/reducers.js
@@ -1,9 +1,16 @@
 const initialState = {
-
+  cards: []
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case 'ADD_CARDS':
+    return {
+      ...state,
+      cards: [
+        ...action.cards,
+      ]
+    }
     default:
     return state
   }
